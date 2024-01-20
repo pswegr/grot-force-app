555955526072e2bbd090840e01871e54f48a7b2c
Pawel W <pswegr@hotmail.com>
2024-01-21 00:50:17 +0100

    add hero component

--- src/app/hero/hero.module.ts
+++ src/app/hero/hero.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroRoutingModule } from './hero-routing.module';
+import { HeroComponent } from './hero.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeroComponent
+  ],
   imports: [
     CommonModule,
     HeroRoutingModule
